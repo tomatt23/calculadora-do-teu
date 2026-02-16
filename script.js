e30cb8b94812336045fad6724c74ce07843c0ec0
tomatt23 <ma2037051@gmail.com>
2026-02-16 13:17:49 -0300

    Add basic calculator functions in script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+{
+document.querySelector('#display').value += data
+}
+
+function clean() {
+  document.querySelector('#display').value = ""
+}
+
+function backspace() {
+  const display = document.querySelector('#display')
+  display.value = display.value.slice(0, -1)
+}
+
+function resultado() {
+  const display = document.querySelector('#display')
+  try {
+    display.value = eval(display.value)
+  } catch {
+    display.value = 'Erro'
+  }
+}
